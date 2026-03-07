ee0dd35ebb4a7e75f2a5c8f637d3c0f5ab57a405
Nagireddy Patil <nagireddy.patil@gmail.com>
2026-03-07 23:23:44 +0530

    List_1

---
+++ bicycles.py
@@ -0,0 +1,12 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+# Adding the negative index to access the last item in the list
+print('Negatibe Indexing')
+print(bicycles[-2])
+print('End')
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
