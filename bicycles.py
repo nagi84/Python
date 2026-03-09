388abffda60b9f9e0af1e675c5faf32d40a91377
Nagireddy Patil <nagireddy.patil@gmail.com>
2026-03-09 13:50:37 +0530

    fix: Correct typo in print statement and add message for new items

--- bicycles.py
+++ bicycles.py
@@ -5,8 +5,9 @@ print(bicycles[0].title())
 print(bicycles[1])
 print(bicycles[3])
 # Adding the negative index to access the last item in the list
-print('Negatibe Indexing')
+print('Negative Indexing')
 print(bicycles[-2])
 print('End')
 message = f"My first bicycle was a {bicycles[0].title()}."
 print(message)
+print('Adding new items to the list')
